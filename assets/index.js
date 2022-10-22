ebc6a1201706ccc68c4288d6e48e22e6826819e6
lherna1371 <lherna1371@gmail.com>
2022-10-22 11:50:57 -0500

    for yuour review

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+// console.log('Hi')
+
+let header = document.querySelector('#chi')
+
+
+let image = document.querySelector('img')
+
+let mainHeader = document.querySelector('h1')
+
+console.log(mainHeader)
